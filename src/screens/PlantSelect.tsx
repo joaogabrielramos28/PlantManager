13545f1b83e219a058c6e6828e7fe1aa1bb8e50e
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-09-07 19:29:50 -0300

    ✨ feat: Load component + loading more on PlantSelect list

--- src/screens/PlantSelect.tsx
+++ src/screens/PlantSelect.tsx
@@ -1,7 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import { EnvironmentButton } from "../components/EnvironmentButton";
 import { Header } from "../components/Header";
+import { Load } from "../components/Load";
 import { PlantCardPrimary } from "../components/PlantCard";
 import api from "../services/api";
 import { colors } from "../styles/colors";
@@ -28,23 +35,56 @@ interface PlantProps {
 export function PlantSelect() {
   const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
   const [plants, setPlants] = useState<PlantProps[]>([]);
+  const [filteredPlants, setFilteredPlants] = useState<PlantProps[]>([]);
   const [environmentSelected, setEnvironmentSelected] = useState("all");
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [loadedAll, setLoadedAll] = useState(false);
 
   function handleEnvironmentSelected(environment: string) {
     setEnvironmentSelected(environment);
+
+    if (environment === "all") return setFilteredPlants(plants);
+
+    const filtered = plants.filter((plant) =>
+      plant.environments.includes(environment)
+    );
+
+    setFilteredPlants(filtered);
   }
 
-  const filtered =
-    environmentSelected === "all"
-      ? plants
-      : plants.filter((plant) =>
-          plant.environments.includes(environmentSelected)
-        );
+  async function fetchPlants() {
+    const { data } = await api.get(
+      `plants?_sort=name&_order=asc&_page=${page}&_limit=8`
+    );
+
+    if (!data) return setLoading(true);
+
+    if (page > 1) {
+      setPlants((oldValue) => [...oldValue, ...data]);
+      setFilteredPlants((oldValue) => [...oldValue, ...data]);
+    } else {
+      setPlants(data);
+      setFilteredPlants(data);
+    }
+
+    setLoading(false);
+    setLoadingMore(false);
+  }
+
+  function handleFetchMore(distance: number) {
+    if (distance < 1) return;
+
+    setLoadingMore(true);
+    setPage((oldValue) => oldValue + 1);
+    fetchPlants();
+  }
 
   useEffect(() => {
     async function fetchEnvironment() {
       const { data } = await api.get(
-        "/plants_environments?_sort=title&_order=asc"
+        "plants_environments?_sort=title&_order=asc"
       );
 
       setEnvironments([
@@ -55,17 +95,16 @@ export function PlantSelect() {
         ...data,
       ]);
     }
+
     fetchEnvironment();
   }, []);
 
   useEffect(() => {
-    async function fetchPlants() {
-      const { data } = await api.get("/plants?_sort=name&_order=asc");
-
-      setPlants(data);
-    }
     fetchPlants();
   }, []);
+
+  if (loading) return <Load />;
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -77,6 +116,7 @@ export function PlantSelect() {
       <View>
         <FlatList
           data={environments}
+          keyExtractor={(item) => String(item.key)}
           renderItem={({ item }) => (
             <EnvironmentButton
               title={item.title}
@@ -92,10 +132,18 @@ export function PlantSelect() {
 
       <View style={styles.plants}>
         <FlatList
-          data={filtered}
+          data={filteredPlants}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => <PlantCardPrimary data={item} />}
           numColumns={2}
           showsVerticalScrollIndicator={false}
+          onEndReachedThreshold={0.1}
+          onEndReached={({ distanceFromEnd }) => {
+            handleFetchMore(distanceFromEnd);
+          }}
+          ListFooterComponent={
+            loadingMore ? <ActivityIndicator color={colors.green} /> : <></>
+          }
         />
       </View>
     </View>
