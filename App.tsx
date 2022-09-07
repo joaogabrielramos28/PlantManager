ace6bd9b2168c6ece4074acde1ce678f9dad8cc4
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-09-07 16:32:33 -0300

    ✨ feat: Finishing welcome screen

--- App.tsx
+++ App.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import { Welcome } from "./src/screens/Welcome";
 
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
+import { ActivityIndicator } from "react-native";
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
   return <Welcome />;
 }
